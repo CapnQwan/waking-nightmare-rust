2370e06bb703b6a399d0d90cb3fab93002f2f8ce
Quin <capn.qwan1519@gmail.com>
2025-09-04 18:02:38 +1200

    Got the context rendering the previous editor layout

--- editor/src/editor/editor_window/editor_window.rs
+++ editor/src/editor/editor_window/editor_window.rs
@@ -1,11 +1,4 @@
-use eframe::{egui};
-use egui::{
-  Color32, Pos2, Rect,
-  containers::{Frame},
-  emath, epaint,
-  epaint::PathStroke,
-  pos2, vec2,
-};
+use crate::editor::editor::Editor;
 
 pub struct EditorWindow {
 }
@@ -18,14 +11,14 @@ impl Default for EditorWindow {
 }
 
 impl EditorWindow {
-  pub fn render_window(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-    self.render_file_system(ctx, frame);
-    self.render_inspector(ctx, frame);
-    self.render_scene(ctx, frame);
-    self.render_scene_structure(ctx, frame);
+  pub fn render_window(&mut self, ctx: &egui::Context, clear_color: &mut [f32; 3]) {
+    self.render_file_system(ctx);
+    self.render_inspector(ctx, clear_color);
+    self.render_scene(ctx);
+    self.render_scene_structure(ctx);
   }
 
-  fn render_scene_structure(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+  fn render_scene_structure(&mut self, ctx: &egui::Context) {
     egui::SidePanel::left("left_panel")
       .resizable(true)
       .default_width(150.0)
@@ -40,7 +33,7 @@ impl EditorWindow {
       });
   }
 
-  fn render_inspector(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+  fn render_inspector(&mut self, ctx: &egui::Context, clear_color: &mut [f32; 3]) {
     egui::SidePanel::right("right_panel")
       .resizable(true)
       .default_width(150.0)
@@ -48,13 +41,12 @@ impl EditorWindow {
       .show(ctx, |ui| {
         ui.vertical_centered(|ui| {
           ui.heading("Inspector");
-        });
-        egui::ScrollArea::vertical().show(ui, |_ui| {
+          ui.color_edit_button_rgb(clear_color.as_mut().try_into().unwrap());
         });
       });
   }
 
-  fn render_file_system(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+  fn render_file_system(&mut self, ctx: &egui::Context) {
     egui::TopBottomPanel::bottom("bottom_panel")
       .resizable(true)
       .default_height(150.0)
@@ -68,43 +60,43 @@ impl EditorWindow {
       });
   }
 
-  fn render_scene(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-    egui::CentralPanel::default().show(ctx, |ui| {
-      Frame::canvas(ui.style()).show(ui, |ui| {
-            ui.ctx().request_repaint();
-            let time = ui.input(|i| i.time);
+  fn render_scene(&mut self, ctx: &egui::Context) {
+    // egui::CentralPanel::default().show(ctx, |ui| {
+    //   Frame::canvas(ui.style()).show(ui, |ui| {
+    //         ui.ctx().request_repaint();
+    //         let time = ui.input(|i| i.time);
 
-            let desired_size = ui.available_width() * vec2(1.0, 0.35);
-            let (_id, rect) = ui.allocate_space(desired_size);
+    //         let desired_size = ui.available_width() * vec2(1.0, 0.35);
+    //         let (_id, rect) = ui.allocate_space(desired_size);
 
-            let to_screen =
-                emath::RectTransform::from_to(Rect::from_x_y_ranges(0.0..=1.0, -1.0..=1.0), rect);
+    //         let to_screen =
+    //             emath::RectTransform::from_to(Rect::from_x_y_ranges(0.0..=1.0, -1.0..=1.0), rect);
 
-            let mut shapes = vec![];
+    //         let mut shapes = vec![];
 
-            for &mode in &[2, 3, 5] {
-                let mode = mode as f64;
-                let n = 120;
-                let speed = 1.5;
+    //         for &mode in &[2, 3, 5] {
+    //             let mode = mode as f64;
+    //             let n = 120;
+    //             let speed = 1.5;
 
-                let points: Vec<Pos2> = (0..=n)
-                    .map(|i| {
-                        let t = i as f64 / (n as f64);
-                        let amp = (time * speed * mode).sin() / mode;
-                        let y = amp * (t * std::f64::consts::TAU / 2.0 * mode).sin();
-                        to_screen * pos2(t as f32, y as f32)
-                    })
-                    .collect();
+    //             let points: Vec<Pos2> = (0..=n)
+    //                 .map(|i| {
+    //                     let t = i as f64 / (n as f64);
+    //                     let amp = (time * speed * mode).sin() / mode;
+    //                     let y = amp * (t * std::f64::consts::TAU / 2.0 * mode).sin();
+    //                     to_screen * pos2(t as f32, y as f32)
+    //                 })
+    //                 .collect();
 
-                let thickness = 10.0 / mode as f32;
-                shapes.push(epaint::Shape::line(
-                    points,
-                    PathStroke::new(thickness, Color32::from_rgb(0x20, 0x21, 0x22)),
-                ));
-            }
+    //             let thickness = 10.0 / mode as f32;
+    //             shapes.push(epaint::Shape::line(
+    //                 points,
+    //                 PathStroke::new(thickness, Color32::from_rgb(0x20, 0x21, 0x22)),
+    //             ));
+    //         }
 
-            ui.painter().extend(shapes);
-        });
-    });
+    //         ui.painter().extend(shapes);
+    //     });
+    // });
   }
 }
--- editor/src/lib.rs
+++ editor/src/lib.rs
@@ -1,16 +1,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 #![allow(rustdoc::missing_crate_level_docs)] // it's an example
 
-use eframe::{UserEvent};
 use winit::event_loop::EventLoop;
 use crate::editor::editor::Editor;
 
 mod editor;
 
 pub fn init() {
-  let event_loop = EventLoop::<UserEvent>::with_user_event()
-      .build()
-      .unwrap();
+  let event_loop = EventLoop::<editor::editor::UserEvent>::with_user_event()
+    .build()
+    .unwrap();
   let proxy = event_loop.create_proxy();
 
   let mut app = Editor::new(proxy);
