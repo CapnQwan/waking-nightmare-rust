6318181be183400ca8585f32ceecc4340c0077e4
CapnQwan <q.p1453@gmail.com>
2025-09-28 13:15:32 +1300

    Setup the mesh_renderer context

--- core/src/rendering/renderer_subsystems/mesh_renderer.rs
+++ core/src/rendering/renderer_subsystems/mesh_renderer.rs
@@ -1,6 +1,10 @@
 use std::rc::Rc;
 
-use crate::{gl::Gles2, rendering::utils::Mesh};
+use crate::{
+  gl::Gles2,
+  math::{Vector2, Vector3},
+  rendering::utils::Mesh,
+};
 
 pub struct MeshRenderer {
   gl: Rc<Gles2>,
@@ -11,6 +15,103 @@ impl MeshRenderer {
     Self { gl }
   }
 
-  pub fn draw_mesh(mesh: &Mesh) {}
-  pub fn bind_mesh_buffers(mesh: &Mesh) {}
+  pub fn draw_mesh(&self, mesh: &Mesh) {
+    unsafe {
+      self.gl.BindVertexArray(mesh.vao);
+      self.gl.DrawElements(
+        gl::TRIANGLES,
+        mesh.triangles.len() as i32,
+        gl::UNSIGNED_INT,
+        std::ptr::null(),
+      );
+      self.gl.BindVertexArray(0);
+    }
+  }
+
+  pub fn bind_mesh_buffers(&self, mesh: &mut Mesh) {
+    unsafe {
+      // @perf_test - does the GenVertexArrays need to run every time the mesh is bound?
+      // How much of an impact does it have to render times?
+
+      // Generate and bind VAO
+      self.gl.GenVertexArrays(1, &mut mesh.vao);
+      self.gl.BindVertexArray(mesh.vao);
+
+      // --- Vertex buffer ---
+      self.gl.GenBuffers(1, &mut mesh.vbo);
+      self.gl.BindBuffer(gl::ARRAY_BUFFER, mesh.vbo);
+      self.gl.BufferData(
+        gl::ARRAY_BUFFER,
+        (mesh.verticies.len() * std::mem::size_of::<Vector3>()) as isize,
+        mesh.verticies.as_ptr() as *const _,
+        gl::STATIC_DRAW,
+      );
+      self.gl.EnableVertexAttribArray(0);
+      self.gl.VertexAttribPointer(
+        0,
+        3,
+        gl::FLOAT,
+        gl::FALSE,
+        std::mem::size_of::<Vector3>() as i32,
+        std::ptr::null(),
+      );
+
+      // --- UV buffer ---
+      if !mesh.uvs.is_empty() {
+        self.gl.GenBuffers(1, &mut mesh.uvbo);
+        self.gl.BindBuffer(gl::ARRAY_BUFFER, mesh.uvbo);
+        self.gl.BufferData(
+          gl::ARRAY_BUFFER,
+          (mesh.uvs.len() * std::mem::size_of::<Vector2>()) as isize,
+          mesh.uvs.as_ptr() as *const _,
+          gl::STATIC_DRAW,
+        );
+        self.gl.EnableVertexAttribArray(1);
+        self.gl.VertexAttribPointer(
+          1,
+          2,
+          gl::FLOAT,
+          gl::FALSE,
+          std::mem::size_of::<Vector2>() as i32,
+          std::ptr::null(),
+        );
+      }
+
+      // --- Normal buffer ---
+      if !mesh.normals.is_empty() {
+        self.gl.GenBuffers(1, &mut mesh.nbo);
+        self.gl.BindBuffer(gl::ARRAY_BUFFER, mesh.nbo);
+        self.gl.BufferData(
+          gl::ARRAY_BUFFER,
+          (mesh.normals.len() * std::mem::size_of::<Vector3>()) as isize,
+          mesh.normals.as_ptr() as *const _,
+          gl::STATIC_DRAW,
+        );
+        self.gl.EnableVertexAttribArray(2);
+        self.gl.VertexAttribPointer(
+          2,
+          3,
+          gl::FLOAT,
+          gl::FALSE,
+          std::mem::size_of::<Vector3>() as i32,
+          std::ptr::null(),
+        );
+      }
+
+      // --- Index buffer ---
+      self.gl.GenBuffers(1, &mut mesh.ibo);
+      self.gl.BindBuffer(gl::ELEMENT_ARRAY_BUFFER, mesh.ibo);
+      self.gl.BufferData(
+        gl::ELEMENT_ARRAY_BUFFER,
+        (mesh.triangles.len() * std::mem::size_of::<u32>()) as isize,
+        mesh.triangles.as_ptr() as *const _,
+        gl::STATIC_DRAW,
+      );
+
+      // Unbind VAO
+      self.gl.BindVertexArray(0);
+    }
+
+    mesh.has_changed = false;
+  }
 }
--- core/src/rendering/utils/mesh.rs
+++ core/src/rendering/utils/mesh.rs
@@ -1,43 +1,27 @@
-use crate::{
-  gl::Gles2,
-  math::{Vector2, Vector3},
-};
+use crate::math::{Vector2, Vector3};
 
 pub struct Mesh {
-  pub triangles: Vec<u32>,
-  pub verticies: Vec<Vector3>,
-  pub uvs: Vec<Vector2>,
-  pub normals: Vec<Vector3>,
+  pub(crate) triangles: Vec<u32>,
+  pub(crate) verticies: Vec<Vector3>,
+  pub(crate) uvs: Vec<Vector2>,
+  pub(crate) normals: Vec<Vector3>,
 
-  // @todo - should these be pub or get a getter for and unmutable
-  // ref be created instead
-  pub vao: gl::types::GLuint,
-  pub vbo: gl::types::GLuint,
-  pub uvbo: gl::types::GLuint,
-  pub nbo: gl::types::GLuint,
-  pub ibo: gl::types::GLuint,
+  pub(crate) vao: gl::types::GLuint,
+  pub(crate) vbo: gl::types::GLuint,
+  pub(crate) uvbo: gl::types::GLuint,
+  pub(crate) nbo: gl::types::GLuint,
+  pub(crate) ibo: gl::types::GLuint,
 
-  pub has_changed: bool,
+  pub(crate) has_changed: bool,
 }
 
 impl Mesh {
-  pub fn new(gl: &Gles2) -> Mesh {
-    let mut vao: gl::types::GLuint = 0;
-    let mut ibo: gl::types::GLuint = 0;
-    let mut vbo: gl::types::GLuint = 0;
-    let mut uvbo: gl::types::GLuint = 0;
-    let mut nbo: gl::types::GLuint = 0;
-
-    unsafe {
-      // Generate vertex array object
-      gl.GenVertexArrays(1, &mut vao);
-
-      // Generate buffers
-      gl.GenBuffers(1, &mut ibo);
-      gl.GenBuffers(1, &mut vbo);
-      gl.GenBuffers(1, &mut uvbo);
-      gl.GenBuffers(1, &mut nbo);
-    }
+  pub fn new() -> Mesh {
+    let vao: gl::types::GLuint = 0;
+    let ibo: gl::types::GLuint = 0;
+    let vbo: gl::types::GLuint = 0;
+    let uvbo: gl::types::GLuint = 0;
+    let nbo: gl::types::GLuint = 0;
 
     Mesh {
       triangles: Vec::new(),
@@ -51,6 +35,26 @@ impl Mesh {
       nbo,
       has_changed: true,
     }
+  }
+
+  pub fn vao(&self) -> gl::types::GLuint {
+    self.vao
+  }
+  pub fn vbo(&self) -> gl::types::GLuint {
+    self.vbo
+  }
+  pub fn uvbo(&self) -> gl::types::GLuint {
+    self.uvbo
+  }
+  pub fn nbo(&self) -> gl::types::GLuint {
+    self.nbo
+  }
+  pub fn ibo(&self) -> gl::types::GLuint {
+    self.ibo
+  }
+
+  pub fn has_changed(&self) -> bool {
+    self.has_changed
   }
 
   pub fn set_vertices(&mut self, verticies: Vec<Vector3>) -> &mut Self {
