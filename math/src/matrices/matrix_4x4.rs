35b486986e68b09766d19a4af0251e2862dabb60
CapnQwan <q.p1453@gmail.com>
2025-10-08 22:03:45 +1300

    Matrix calc update

--- math/src/matrices/matrix_4x4.rs
+++ math/src/matrices/matrix_4x4.rs
@@ -20,10 +20,10 @@ impl Matrix4x4 {
   #[rustfmt::skip]
   fn to_column_major_arr(&self) -> [f32; 16] {
     [
-      self[0][0], self[0][0], self[0][0], self[0][0], 
-      self[0][0], self[0][0], self[0][0], self[0][0], 
-      self[0][0], self[0][0], self[0][0], self[0][0], 
-      self[0][0], self[0][0], self[0][0], self[0][0],
+      self[0][0], self[1][0], self[2][0], self[3][0], 
+      self[0][1], self[1][1], self[2][1], self[3][1], 
+      self[0][2], self[1][2], self[2][2], self[3][2], 
+      self[0][3], self[1][3], self[2][3], self[3][3],
     ]
   }
 }
