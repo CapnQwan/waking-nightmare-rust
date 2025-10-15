22009fc607545eb05790e3455b5f90a616c13527
Quin <capn.qwan1519@gmail.com>
2025-10-16 09:04:45 +1300

    Copied lit shaders across from old typescript version of waking nightmare

---
+++ core/src/assets/shaders/default_lit_frag.rs
@@ -0,0 +1,56 @@
+// @todo - replace this with loading glsl files instead
+pub const FRAGMENT_SHADER_SOURCE: &[u8] = b"
+#version 300 es
+precision mediump float;
+
+// Declare an output variable for the fragment color
+in vec3 vNormal;
+in vec3 vPosition;
+
+// @todo - add support for lit materials
+// uniform vec3 uLightPosition;
+// uniform vec3 uLightAmbient;
+// uniform vec3 uLightDiffuse;
+// uniform vec3 uLightSpecular;
+
+// uniform vec3 uMaterialAmbient;
+// uniform vec3 uMaterialDiffuse;
+// uniform vec3 uMaterialSpecular;
+// uniform float uMaterialShininess;
+
+uniform vec3 uViewPosition;
+
+out vec4 fragColor;
+
+void main() {
+  vec3 uLightPosition = vec3(-2.0, 2.0, -5.0);
+  vec3 uLightAmbient = vec3(0.5);
+  vec3 uLightDiffuse = vec3(0.2);
+  vec3 uLightSpecular = vec3(0.75);
+  vec3 uMaterialAmbient = vec3(0.2);
+  vec3 uMaterialDiffuse = vec3(0.8);
+  vec3 uMaterialSpecular = vec3(1.0);
+  float uMaterialShininess = 0.5;
+
+
+  vec3 normal = normalize(vNormal);
+  vec3 lightDir = normalize(uLightPosition - vPosition);
+  vec3 viewDir = normalize(uViewPosition - vPosition);
+  vec3 reflectDir = reflect(-lightDir, normal);
+
+  // Ambient
+  vec3 ambient = uLightAmbient * uMaterialAmbient;
+
+  // Diffuse
+  float diff = max(dot(normal, lightDir), 0.0);
+  vec3 diffuse = uLightDiffuse * uMaterialDiffuse * diff;
+
+  // Specular
+  float spec = pow(max(dot(viewDir, reflectDir), 0.0), uMaterialShininess);
+  vec3 specular = uLightSpecular * uMaterialSpecular * spec;
+
+  // Combine components
+  vec3 color = ambient + diffuse + specular;
+  fragColor = vec4(color, 1.0);
+}
+\0";
---
+++ core/src/assets/shaders/default_lit_vert.rs
@@ -0,0 +1,23 @@
+// @todo - replace this with loading glsl files instead
+pub const VERTEX_SHADER_SOURCE: &[u8] = b"
+#version 300 es
+precision mediump float;
+
+layout(location = 0) in vec3 aPosition;
+layout(location = 1) in vec3 aNormal;
+
+out vec3 vNormal;
+out vec3 vPosition;
+
+uniform mat3 uNormalMatrix;
+uniform mat4 uModelMatrix;
+uniform mat4 uViewMatrix;
+uniform mat4 uProjectionMatrix;
+
+void main() {
+  mat4 modelViewMatrix = uViewMatrix * uModelMatrix;
+  gl_Position = uProjectionMatrix * modelViewMatrix * vec4(aPosition, 1.0);
+  vNormal = aNormal * uNormalMatrix;
+  vPosition = (modelViewMatrix * vec4(aPosition, 1.0)).xyz;
+}
+\0";
